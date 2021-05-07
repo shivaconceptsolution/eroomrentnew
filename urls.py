17c2814828b66509db6d9c3b444e020320293de4
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2021-05-07 18:06:40 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,30 @@
+"""eroomrent URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('userapp/',include('userapp.urls')),
+    path('swap/',include('swap.urls')),
+    path('addition/',include('addition.urls')),
+    path('guestapp/', include('guestapp.urls')),
+    path('tenantapp/',include('tenantapp.urls')),
+    path('ownerapp/',include('ownerapp.urls')),
+    path('admin/', admin.site.urls),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
